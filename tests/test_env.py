0718c5e3d1b024753f3a67cf72cc3d4cc69ca666
nushab21 <bhat.anu@northeastern.edu>
2026-04-04 12:50:49 -0400

    update tests file api fix

--- tests/test_env.py
+++ tests/test_env.py
@@ -2,17 +2,17 @@ from src.env.project_env import ProjectEnv
 
 env = ProjectEnv()
 
-obs = env.reset()
+obs, _ = env.reset()
 print("Initial observation:", obs)
 
 for i in range(10):
     action = env.action_space.sample()  # random action
-    obs, reward, done, info = env.step(action)
+    obs, reward, terminated, truncated, info = env.step(action)
 
     print(f"Step {i}")
     print("Obs:", obs)
     print("Reward:", reward)
 
-    if done:
+    if terminated or truncated:
         print("Episode ended early")
         break
